f5cf18ee02ac52674be11045ebf8d22f12e61cdf
FlutLab.io <>
2022-07-02 03:48:47 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -6,7 +7,6 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InputPage(),
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         backgroundColor: Color(0xFF0A0E21),
@@ -15,29 +15,7 @@ class BMICalculator extends StatelessWidget {
           color: Color(0xFF0A0E21),
         ),
       ),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
+      home: InputPage(),
     );
   }
 }
