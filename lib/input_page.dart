c1a1881c1be0bf15910db5fe61f2eb7c8652012c
FlutLab.io <>
2022-07-05 20:57:49 +0000

    Autosave

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
@@ -19,25 +20,63 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Column(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  // color: const Color(0xF1D1E33),
-                  color: Colors.red,
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    // color: const Color(0xF1D1E33),
+                    color: Colors.red,
+                  ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  // color: const Color(0xF1D1E33),
-                  color: Colors.white,
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    // color: const Color(0xF1D1E33),
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    // color: const Color(0xF1D1E33),
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    // color: const Color(0xF1D1E33),
+                    color: Colors.red,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(15.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    // color: const Color(0xF1D1E33),
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
