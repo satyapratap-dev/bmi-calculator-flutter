c9131a9bc06468507231da5c1161da46e2e41664
FlutLab.io <>
2022-07-05 20:17:02 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class BMICalculator extends StatelessWidget {
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         backgroundColor: Color(0xFF0A0E21),
-        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        // scaffoldBackgroundColor: Color(0xFF0A0E21),
         appBarTheme: AppBarTheme(
           color: Color(0xFF0A0E21),
         ),
