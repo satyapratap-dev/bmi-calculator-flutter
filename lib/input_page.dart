5fc49d160654a6883feb492da43c10a8056f1a97
FlutLab.io <>
2022-07-05 20:19:57 +0000

    Autosave

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -28,7 +28,8 @@ class _InputPageState extends State<InputPage> {
               margin: EdgeInsets.all(15.0),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10.0),
-                color: const Color(0xF1D1E33),
+                // color: const Color(0xF1D1E33),
+                color: Colors.red,
               ),
             ),
           ),
