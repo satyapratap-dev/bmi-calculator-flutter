a22c354c9527eaa005c5f57b7e7b7fe67960e102
FlutLab.io <>
2022-07-05 20:54:15 +0000

    Autosave

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -19,28 +19,20 @@ class _InputPageState extends State<InputPage> {
       ),
       body: Column(
         children: <Widget>[
-          Expanded(
-            child: Row(
-              children: Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    // color: const Color(0xF1D1E33),
-                    color: Colors.red,
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                    // color: const Color(0xF1D1E33),
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+          Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              // color: const Color(0xF1D1E33),
+              color: Colors.red,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              // color: const Color(0xF1D1E33),
+              color: Colors.white,
             ),
           ),
         ],
@@ -48,3 +40,14 @@ class _InputPageState extends State<InputPage> {
     );
   }
 }
+
+// Expanded(
+//                 child: Container(
+//                   margin: EdgeInsets.all(15.0),
+//                   decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.circular(10.0),
+//                     // color: const Color(0xF1D1E33),
+//                     color: Colors.red,
+//                   ),
+//                 ),
+//               ),
