b042c4696ee1ab05af3210d985b45d6ea32b1a6a
FlutLab.io <>
2022-07-06 17:24:57 +0000

    Autosave

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -33,17 +33,17 @@ class _InputPageState extends State<InputPage> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: <Widget>[
                         Icon(
-                          Icons.mood,
-                          size: 15.0,
+                          Icons.male,
+                          size: 80.0,
                         ),
                         SizedBox(
-                          height: 10.0,
+                          height: 15.0,
                         ),
                         Text(
                           "MALE",
                           style: TextStyle(
-                            fontSize: 15.0,
-                            color: Colors.white,
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
                           ),
                         ),
                       ],
