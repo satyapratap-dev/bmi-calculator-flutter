69f932d11a3b2e406df904883e310964dc4ed4f0
FlutLab.io <>
2022-07-10 04:05:40 +0000

    Autosave

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -90,7 +90,8 @@ class _InputPageState extends State<InputPage> {
                     data: SliderTheme.of(context).copyWith(
                       activeTrackColor: Colors.white,
                       inactiveTrackColor: Color(0xFF8D8E98),
-                      thumbColor: Color(0x29EB1555),
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
                       thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
                       overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
                     ),
